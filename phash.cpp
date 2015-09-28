c6d06f88eb3bcd6a3d010c0ae040bde4536871a2
Dim0N22 <dim0000n@gmail.com>
2015-09-28 14:27:25 +0300

    Update phash.cpp

--- phash.cpp
+++ phash.cpp
@@ -23,6 +23,7 @@ ulong64 pc_dct_imagehash_Wrapper(const char *file) {
 
 ulong64* pc_dct_videohash_Wrapper(const char *file) {
     cimg::exception_mode(0);
+    printf("%i\n", length);
     int length;
 
     ulong64* h = ph_dct_videohash(file, length); 
