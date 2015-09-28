def03fa18907a0040213f3f09d25f8e29f3ac140
Dim0N22 <dim0000n@gmail.com>
2015-09-28 12:19:49 +0300

    Update phash.cpp

--- phash.cpp
+++ phash.cpp
@@ -30,7 +30,7 @@ ulong64* pc_dct_videohash_Wrapper(const char *file) {
     
     printf("%s\n", "1");
     printf("%i\n", length);
-    std::count<<h == NULL;
+    std::cout<<h == NULL;
     
     for (int i = 0; i < length; i++) 
         std::cout << *(h+i);
