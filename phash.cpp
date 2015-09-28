71a5714b87717bb4a00e3bfc5c33e8e07df0685c
Dim0N22 <dim0000n@gmail.com>
2015-09-28 11:54:56 +0300

    Update phash.cpp

--- phash.cpp
+++ phash.cpp
@@ -5,6 +5,7 @@
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <pHash.h>
+#include <iostream>
 
 #ifdef __cplusplus
 extern "C" {
@@ -26,7 +27,8 @@ ulong64* pc_dct_videohash_Wrapper(const char *file) {
 
     ulong64* h = ph_dct_videohash(file, length); 
     errno = 0;
-    printf("%p\n", &h);
+    for (int i = length - 1; i >= 0; i--) 
+        cout << h[i];
     
     return h;
 }
