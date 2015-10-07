945e3e069b721cad08e5c99a925b6a16efc15cce
Dim0N22 <dim0000n@gmail.com>
2015-10-07 12:06:21 +0300

    Update phash.go

--- phash.go
+++ phash.go
@@ -52,7 +52,7 @@ func VideoHashDCT(file string) ([]uint64, error) {
 }
 
 func HammingDistanceForVideoHashes(hashA []uint64, hashB []uint64, threshold int) (float64, error) {
-	distance, err := C.ph_dct_videohash_dist((*C.ulong64)(unsafe.Pointer(&hashA[0])), C.int(len(hashA)), (*C.ulong64)(unsafe.Pointer(&hashB[0])), C.int(len(hashB)), threshold)
+	distance, err := C.ph_dct_videohash_dist((*C.ulong64)(unsafe.Pointer(&hashA[0])), C.int(len(hashA)), (*C.ulong64)(unsafe.Pointer(&hashB[0])), C.int(len(hashB)), C.int(threshold))
 
 	return float64(distance), err
 }
